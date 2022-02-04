fa608db75b3e7801000d726f381814fb62b506c0
Erin Van Brunt <erinmontybruce@gmail.com>
2022-02-04 13:28:36 -0800

    completed

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(val1, val2) {
+  return val1 + val2;
+}
+function subtract(val1, val2) {
+  return val1 - val2;
+}
+function multiply(val1, val2) {
+  return val1 * val2;
+}
+function divide(val1, val2) {
+  return val1 / val2;
+}
+function increment(n) {
+  return n + 1;
+}
+function decrement(n) {
+  return n - 1;
+}
+function makeInt(n) {
+  let r = 10;
+  return parseInt(n, r);
+}
+function preserveDecimal(n) {
+  n = parseFloat(n);
+  return n;
+}
